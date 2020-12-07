a2f0434f30e2b3b52f994875a0eb1407eed9c15a
rup9823 <rupeshkumar.9823@gmail.com>
2020-12-07 16:09:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-kchn7p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-kchn7p)
---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  VERSION,
+  ViewChildren
+} from "@angular/core";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+var orderId = 10000;
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  name = "Angular " + VERSION.major;
+  addForm: FormGroup;
+  items = new Array();
+  edit_order_id: Number;
+  @ViewChildren("items") products: QueryList<ElementRef>;
+  constructor(private formBuilder: FormBuilder) {
+    this.addForm = formBuilder.group({
+      item: new FormControl(),
+      price: new FormControl(),
+      quantity: new FormControl()
+    });
+  }
+
+  ngOnInit(): void {}
+  addData() {
+    let quantity = this.addForm.value.quantity;
+    let price = this.addForm.value.price;
+    let total = quantity * price;
+    this.items.push({
+      date: new Date(),
+      orderId: orderId--,
+      itemName: this.addForm.value.item,
+      price: price,
+      quantity: quantity,
+      total: total
+    });
+    console.log(this.items);
+  }
+  editData() {
+    let obj = this.items.find(x => x.orderId === this.edit_order_id);
+    let index = this.items.indexOf(obj);
+    let quantity = this.addForm.value.quantity;
+    let price = this.addForm.value.price;
+    let total = quantity * price;
+    let itemName = this.addForm.value.item;
+    let new_obj = {
+      date: new Date(),
+      orderId: this.edit_order_id,
+      itemName: this.addForm.value.item,
+      price: price,
+      quantity: quantity,
+      total: total
+    };
+    console.log(new_obj);
+    this.items[index] = new_obj;
+    console.log(this.items);
+  }
+  edit_id(items, i) {
+    this.edit_order_id = i.orderId;
+  }
+}
